2c2560a9b0150635844b1957fcda84a458f4b4ad
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2025-08-12 23:10:55 +0500

    fix(ui): disable react/button-has-type rule for Button component with default type

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,3 +1,6 @@
 import * as React from "react";
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
-export const Button: React.FC<ButtonProps> = ({ type = "button", ...props }) => (<button type={type} {...props} />);
+export const Button: React.FC<ButtonProps> = ({ type = "button", ...props }) => (
+  // eslint-disable-next-line react/button-has-type
+  <button type={type} {...props} />
+);
